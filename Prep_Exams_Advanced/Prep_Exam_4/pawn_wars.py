cd1861960d092ce3d571291efb3deae71f2877b8
Milen <feniks9511@gmail.com>
2022-10-20 11:17:04 +0300

    added exercises

--- Prep_Exams_Advanced/Prep_Exam_4/pawn_wars.py
+++ Prep_Exams_Advanced/Prep_Exam_4/pawn_wars.py
@@ -1,34 +1,32 @@
-
-
 board = []
 
-for i in range(8,-1,-1):
+for i in range(8, -1, -1):
     row = []
     for j in range(8):
-        row.append(f'{chr(97+j)}{i}')
+        row.append(f'{chr(97 + j)}{i}')
     board.append(row)
 
-w=[]
-b=[]
+w = []
+b = []
 
 for current_row in range(8):
     row_elements = input().split()
     for current_col in range(len(row_elements)):
         if row_elements[current_col] == 'w':
-            w = [current_row,current_col,'White']
+            w = [current_row, current_col, 'White']
         elif row_elements[current_col] == 'b':
-            b = [current_row,current_col,'Black']
+            b = [current_row, current_col, 'Black']
 
-players = [w,b]
+players = [w, b]
 
 while True:
 
-
-    player_row1,player_col1, name1 = players[0]
-    player_row2,player_col2, name2 = players[1]
+    player_row1, player_col1, name1 = players[0]
+    player_row2, player_col2, name2 = players[1]
 
     if name1 == 'White':
-        if (player_row1 - 1 == player_row2 and player_col1 - 1 == player_col2) or (player_row1 - 1 == player_row2 and player_col1 + 1 == player_col2):
+        if (player_row1 - 1 == player_row2 and player_col1 - 1 == player_col2) or (
+                player_row1 - 1 == player_row2 and player_col1 + 1 == player_col2):
 
             print(f"Game over! {name1} win, capture on {board[player_row2][player_col2]}.")
             break
@@ -39,7 +37,8 @@ while True:
                 break
 
     elif name1 == 'Black':
-        if (player_row1 + 1 == player_row2 and player_col1 - 1 == player_col2) or (player_row1 + 1 == player_row2 and player_col1 + 1 == player_col2):
+        if (player_row1 + 1 == player_row2 and player_col1 - 1 == player_col2) or (
+                player_row1 + 1 == player_row2 and player_col1 + 1 == player_col2):
 
             print(f"Game over! {name1} win, capture on {board[player_row2][player_col2]}.")
             break
