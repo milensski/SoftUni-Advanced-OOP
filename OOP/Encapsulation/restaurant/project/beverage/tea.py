a987c1e453a3b2d13c51c283ce7b5c5a9b8995b3
Milen <feniks9511@gmail.com>
2022-11-11 20:24:17 +0200

    added exercises

---
+++ OOP/Encapsulation/restaurant/project/beverage/tea.py
@@ -0,0 +1,6 @@
+from project.beverage.hot_beverage import HotBeverage
+
+
+class Tea(HotBeverage):
+    pass
+
---
+++ OOP/Encapsulation/restaurant/project/food/soup.py
@@ -0,0 +1,6 @@
+from project.food.starter import Starter
+
+
+class Soup(Starter):
+    pass
+
