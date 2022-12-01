771324e51d97435b5e5867c73b68db27a217a723
Milen <feniks9511@gmail.com>
2022-12-01 17:29:09 +0200

    Update test_mammal.py

--- OOP/Testing/mammal/test/test_mammal.py
+++ OOP/Testing/mammal/test/test_mammal.py
@@ -16,6 +16,22 @@ class TestMammal(TestCase):
         self.assertEqual('hugh',self.mammal.sound)
         self.assertEqual('animals', self.mammal._Mammal__kingdom)
 
+    def test_makesound(self):
+        result = self.mammal.make_sound()
+        expected = "Dayzee makes hugh"
+
+        self.assertEqual(expected, result)
+
+    def test_get_kingdom(self):
+        result = self.mammal.get_kingdom()
+        expected = 'animals'
+        self.assertEqual(expected, result)
+
+
+    def test_info_function(self):
+        result = self.mammal.info()
+        expected = "Dayzee is of type penguin"
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
